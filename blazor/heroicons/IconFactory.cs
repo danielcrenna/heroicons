da5b03d9ee065a179a1c414a9b1cb6a951d004c9
Daniel Crenna <daniel.crenna@gmail.com>
2025-04-11 21:35:23 -0400

    nit: icon type miss could at least reflect style

--- blazor/heroicons/IconFactory.cs
+++ blazor/heroicons/IconFactory.cs
@@ -2,16 +2,25 @@ namespace heroicons;
 
 public static class IconFactory
 {
-    public static IconType DefaultIconType = IconType.QuestionMarkCircle;
-    public static IconStyle DefaultIconStyle = IconStyle.Solid;
-    public static IconSize DefaultIconSize = IconSize.Regular;
-
-    public static Type DefaultIcon = typeof(Regular.Solid.QuestionMarkCircleIcon);
+    internal static IconType DefaultIconType = IconType.QuestionMarkCircle;
+    internal static IconStyle DefaultIconStyle = IconStyle.Solid;
+    internal static IconSize DefaultIconSize = IconSize.Regular;
 
     public static Type GetIconType(IconType type, IconStyle style, IconSize size)
     {
         var typeName = $"{typeof(Icon).Namespace}.{size}.{style}.{type}Icon";
         var iconType = Type.GetType(typeName, false, true);
-        return iconType ?? DefaultIcon;
+        if (iconType != null)
+            return iconType;
+
+        return size switch
+        {
+            IconSize.Regular => style == IconStyle.Solid
+                ? typeof(Regular.Solid.QuestionMarkCircleIcon)
+                : typeof(Regular.Outline.QuestionMarkCircleIcon),
+            IconSize.Mini => typeof(Mini.Solid.QuestionMarkCircleIcon),
+            IconSize.Micro => typeof(Micro.Solid.QuestionMarkCircleIcon),
+            _ => throw new ArgumentOutOfRangeException(nameof(size), size, null)
+        };
     }
 }
